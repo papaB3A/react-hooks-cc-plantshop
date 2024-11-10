73f30fb944a06454ae80a07563d4830add63415b
papaB3A <papa.akumu@student.moringaschool.com>
2024-11-10 12:52:53 +0300

    Fetches all plants on mount, Adds a new plant, Filters plants based on search input

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage() {
-  return (
+function PlantPage(){
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("/plants")
+      .then((res) => res.json())
+      .then((data) => setPlants(data));
+  }, []);
+
+  //this adds a new plant to the PlantList
+  function handleAddPlant(newPlant){
+    setPlants((prevPlants) => [...prevPlants, newPlant]);
+  }
+
+  const filteredPlants = plants.filter((plant) => {
+      //Convert the plant name to lowercase for ease in searching
+      plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+    }
+  );
+
+  return(
+    //render the NewPlantForm, Search bar and PlantList 
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} onSearch={setSearchTerm} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
