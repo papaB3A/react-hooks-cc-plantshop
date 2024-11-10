4b9aa4f95695c2c3b1add5ba60c05c08ea28b137
papaB3A <papa.akumu@student.moringaschool.com>
2024-11-10 13:05:38 +0300

    Updated to retrieve data from http://localhost:6001/plants

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -3,25 +3,25 @@ import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
-function PlantPage(){
+function PlantPage() {
   const [plants, setPlants] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
+  // Fetch data from the backend at port 6001
   useEffect(() => {
-    fetch("/plants")
+    fetch("http://localhost:6001/plants")
       .then((res) => res.json())
       .then((data) => setPlants(data));
   }, []);
 
-  //this adds a new plant to the PlantList
-  function handleAddPlant(newPlant){
+  //this adds a new plant to the plants array
+  function handleAddPlant(newPlant) {
     setPlants((prevPlants) => [...prevPlants, newPlant]);
   }
 
-  const filteredPlants = plants.filter((plant) => {
-      //Convert the plant name to lowercase for ease in searching
-      plant.name.toLowerCase().includes(searchTerm.toLowerCase())
-    }
+  const filteredPlants = plants.filter((plant) =>
+    //Convert the plant name to lowercase for ease in searching
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return(
