d8581c9550826f088d7af68e87f09a94e6d31eb5
papaB3A <papa.akumu@student.moringaschool.com>
2024-11-10 12:41:57 +0300

    Marks a plant as "sold out" when button is clicked

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
-function PlantCard() {
+function PlantCard({ plant }){
+  //create a isSoldOut state...initalized to false
+  const [isSoldOut, setIsSoldOut] = useState(false);
+
+  function handleToggleStock(){
+    //when clicked
+    setIsSoldOut(!isSoldOut);
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: ${plant.price}</p>
+      <button onClick={handleToggleStock} className={isSoldOut ? "" : "primary"}>
+        {isSoldOut ? "Out of Stock" : "In Stock"}
+      </button>
     </li>
   );
 }
